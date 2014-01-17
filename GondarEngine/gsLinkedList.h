127ec1d5be2ddbe38a10345ecbab45e04084ced0
Yvens Rebouças Serpa <yvensre@gmail.com>
2014-01-17 17:32:46 -0300

    Implementado classe de LinkedList
    Classe de LinkedList adicionada.
    Adicionado o filtro para Data Structures.

---
+++ GondarEngine/gsLinkedList.h
@@ -0,0 +1,32 @@
+#ifndef __GLLINKEDLIST__
+#define __GLLINKEDLIST__
+
+#include "gsNode.h"
+
+#define generics template <class T>
+
+generics
+class gsList
+{
+private:
+	int size;
+
+	gsNode<T>* head;
+	void add(gsNode<T>* node);
+	gsNode<T>* getHead(void);
+public:
+	gsList<T>(void);
+	gsList<T>(T value);
+	~gsList(void);
+
+	T get(int i);
+
+	void add(T value);
+	void remove(int i);
+	bool contains(T value);
+	void clear(void);
+
+	int size(void);
+	void log(void);
+};
+#endif
