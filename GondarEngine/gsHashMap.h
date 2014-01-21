3b0c24b60e4fcfd52e607c61f1d688e819825d87
duducorvao <eduardobruno@hotmail.co.uk>
2014-01-21 14:55:24 -0200

    Hashmap Update
    Preciso avaliar o código com alguém e testar para ver se apenas isso
    basta.

--- GondarEngine/gsHashMap.h
+++ GondarEngine/gsHashMap.h
@@ -14,10 +14,12 @@ public:
 	gsHashMap<T>(int size);
 	~gsHashMap<T>(void);
 
-	T get(int i);
+	T get(int value);
+
+	int hashFunction(int key);
 
 	void add(T value);
-	void remove(int i);
+	void remove(int value);
 	bool contains(T value);
 	void clear(void);
 };
@@ -26,8 +28,8 @@ public:
 generics
 gsHashMap<T>::gsHashMap(int size)
 {
-	this->data = new T[size];
-	
+	data = new T[size];
+
 	for (size_t i = 0; i < size; i++)
 	{
 		data[i] = new gsArrayList();
@@ -41,26 +43,39 @@ gsHashMap<T>::~gsHashMap(void)
 }
 
 generics
-T gsHashMap<T>::get(int i)
+T gsHashMap<T>::get(int value)
 {
+	return data[hashFunction(value)]->get(value);
+	//Retorna o objeto T que esta no ArrayList da posicao indicada pela funcao hash
+
 	//Usar o resultado da função de hash, mas a função vai depender do tipo de key do hashmap ?
+}
+
+generics
+int gsHashMap<T>::hashFunction(int value)
+{
+	return value % size; //Função simples para testes.
+
 }
 
 generics
 void gsHashMap<T>::add(T value)
 {
-	//Usar o retorno da função get(int i);
+	data[hashFunction(value)]->add(value);
 }
 
 generics
-void gsHashMap<T>::remove(int i)
-{ 
-
+void gsHashMap<T>::remove(int value)
+{
+	data[hashFunction(value)]->remove(value);
 }
 
 generics
 bool gsHashMap<T>::contains(T value)
 {
+
+	return data[hashFunction(value)]->contains(value);
+
 	/*Usar o resultado da função hash para saber a possivel localização desse valor no hashmap
 	para depois procurar no ArrayList daquela posição se o valor existe.
 	*/
