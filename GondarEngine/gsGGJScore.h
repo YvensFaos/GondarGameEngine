fa1db6b9e51807f429a64d762f468c3c73d9856c
GODZILLA GIROFLEX <jordyfgomes@gmail.com>
2014-01-26 14:11:20 -0300

    Começo da implementação do placar

--- GondarEngine/gsGGJScore.h
+++ GondarEngine/gsGGJScore.h
@@ -1,11 +1,23 @@
 #ifndef __GS_GGJ_SCORE_H__
 #define __GS_GGJ_SCORE_H__
 
-class gsGGJScore
+#include "gsGGJObject.h"
+#include "gsGGJNumber.h"
+#include "gsArrayList.h"
+
+class gsGGJScore : public gsGGJObject
 {
 public:
-	gsGGJScore(void);
-	~gsGGJScore(void);
+	gsArrayList<gsGGJNumber> numbers;
+
+	gsGGJScore(void) {}
+	gsGGJScore(gsGGJGame* game);
+	virtual ~gsGGJScore(void);
+
+	virtual void update();
+	virtual void draw();
+
+	void onCollision(gsGameObject *other, const gsCollisionInfo& info){}
 };
 
 #endif
