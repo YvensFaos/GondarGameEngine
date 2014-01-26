d2745ffb3898c6fc3c7aeb4c34309c2be96b9073
Yvens Rebouças Serpa <yvensre@gmail.com>
2014-01-26 00:47:24 -0200

    Correção do gsGGJNumber

--- GondarEngine/gsGGJNumber.h
+++ GondarEngine/gsGGJNumber.h
@@ -17,12 +17,8 @@ public:
 	virtual void update();
 	virtual void draw();
 
-<<<<<<< HEAD
 	void onCollision(gsGameObject *other, const gsCollisionInfo& info){}
 	void draw(int number,int x, int y);
-=======
-	~gsGGJNumber(void);
->>>>>>> f8ecffa20a5f5efa1354fd6adb6742856874d61b
 };
 
 #endif
